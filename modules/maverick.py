c818c45bccd04a4055cb44d92e2cae706d434a59
Nathan Smith <nathan@praisetopia.org>
2014-07-02 13:11:20 -0500

    removed hello module

--- modules/maverick.py
+++ modules/maverick.py
@@ -2,5 +2,6 @@ from willie import module
 
 @module.rule(r'.*(?i)good morning')
 def maverick(bot, trigger):
+	"""says good morning, but only to maverick or natsmith"""
 	if(trigger.nick == 'maverick' or trigger.nick == 'natsmith'):
 		bot.say('Good morning, ' + trigger.nick)
