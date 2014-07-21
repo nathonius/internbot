d0f3e9b4e587128685aee39b32f0dfa60d100f86
Maverick93 <michael.pollock93@gmail.com>
2014-07-21 14:03:29 -0400

    Update maverick.py

--- modules/maverick.py
+++ modules/maverick.py
@@ -5,3 +5,6 @@ def maverick(bot, trigger):
 	"""says good morning, but only to maverick or natsmith"""
 	if(trigger.nick == 'maverick' or trigger.nick == 'natsmith'):
 		bot.say('Good morning, ' + trigger.nick)
+		bot.say('https://www.youtube.com/watch?v=s1I7D5MsSvU')
+	else
+		bot.say('https://www.youtube.com/watch?v=s1I7D5MsSvU')
